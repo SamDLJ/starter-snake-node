7bc13d95b8b770f06433688a2736cab6511d5d2a
SamDLJ <samdljohnson@gmail.com>
2019-03-02 14:42:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//      http://localhost:9001
+
+
+
 const bodyParser = require('body-parser')
 const express = require('express')
 const logger = require('morgan')
@@ -8,6 +12,24 @@ const {
   genericErrorHandler,
   poweredByHandler
 } = require('./handlers.js')
+const u = 'up'
+const d = 'down'
+const r = 'right'
+const l = 'left'
+var m = r
+var job = 'x'
+var bx = 11
+var by = 11
+var segs
+var wx
+var wy
+var nu = false
+var nd = false
+var nl = false
+var nr = false
+//var segs = [][]
+//var food = [][]
+
 
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
@@ -27,7 +49,7 @@ app.post('/start', (request, response) => {
 
   // Response data
   const data = {
-    color: '#DFFF00',
+    color: '#000000',
   }
 
   return response.json(data)
@@ -36,17 +58,87 @@ app.post('/start', (request, response) => {
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
-
+  // ------------------------------------------------
+	//b = request.body.board
+	nu = false
+	nd = false
+	nl = false
+	nr = false
+	
+	var sx = 0
+	var sy = 0
+	
+	bx = request.body.board.width
+	by = request.body.board.height
+	
+	segs = new Array(by);
+	for (var i = 0; i < segs.length; i++) {
+	  segs[i] = new Array(bx);
+		for (var j=0; j<bx; j++){
+			segs[i][j] = 0;
+		}
+	}
+	
+	numS = request.body.board.snakes.length
+	numF = request.body.board.food.length
+	
+	for (var i=0; i<numS; i++) {
+		slen = request.body.board.snakes[i].body.length
+		for (var seg=0; seg<slen; seg++){
+			sx = request.body.board.snakes[i].body[seg].x;
+		  sy = request.body.board.snakes[i].body[seg].y;
+			segs[sy][sx] = 1
+		}
+	}
+	for (var i=0; i<numF; i++) {
+		fx = request.body.board.food[i].x;
+		fy = request.body.board.food[i].y;
+		segs[fy][fx] = 2
+	}
+	
+	
+	wx = request.body.you.body[0].x
+	wy = request.body.you.body[0].y
+	
+	if (wx == 0) {
+		nl = true
+	}
+	if (wx == bx-1) {
+		nr = true
+	}
+	if (wy == 0) {
+		nu = true
+	}
+	if (wy == by-1) {
+		nd = true
+	}
+	//test = request.body.board.snakes[0].body[0]
+	
+	
+	
+	
+	console.log(wx)
+	
+	
+	
+	
+	
+	
+	
+	// ------------------------------------------------
+	
   // Response data
   const data = {
-    move: 'up', // one of: ['up','down','left','right']
+    move: m, // one of: ['up','down','left','right']
   }
-
+  
   return response.json(data)
 })
 
 app.post('/end', (request, response) => {
   // NOTE: Any cleanup when a game is complete.
+	//print(response)
+	//console.log(segs)
   return response.json({})
 })
 
